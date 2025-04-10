8401f283a56cc462343a1c14ffb7688c8eb1ca24
VasylPopaz <pafos1997@gmail.com>
2025-04-10 17:53:59 +0200

    Add CurrentUser decorator and /me route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 import { CreateUserRequest } from './dto/create-user-request';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { CurrentUser } from '../auth/current-user.decorator';
+import { TokenPayload } from '../auth/token-payload.interface';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +22,11 @@ export class UsersController {
   @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
+  }
+
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  getMe(@CurrentUser() user: TokenPayload) {
+    return user;
   }
 }
