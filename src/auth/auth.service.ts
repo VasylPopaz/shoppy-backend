e587bf25b039871e654795c4db821f9318cda54d
VasylPopaz <pafos1997@gmail.com>
2025-04-11 21:03:14 +0200

    Add AuthService, AuthController and login route

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,57 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import ms from 'ms';
+import { Response } from 'express';
+import { User } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+import { UsersService } from '../users/users.service';
+import { TokenPayload } from './token-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User, response: Response) {
+    const expiresIn = ms(this.configService.getOrThrow('JWT_EXPIRATION'));
+
+    if (typeof expiresIn !== 'number') {
+      throw new Error('Invalid JWT_EXPIRATION format');
+    }
+
+    const expires = new Date();
+    expires.setMilliseconds(expires.getMilliseconds() + expiresIn);
+
+    const tokenPayload: TokenPayload = {
+      userId: user.id,
+    };
+    const token = this.jwtService.sign(tokenPayload);
+
+    response.cookie('Authentication', token, {
+      secure: true,
+      httpOnly: true,
+      expires,
+    });
+    return { tokenPayload };
+  }
+
+  async verifyUser(email: string, password: string) {
+    try {
+      const user = await this.userService.getUser({ email });
+
+      const authenticated = await bcrypt.compare(password, user.password);
+
+      if (!authenticated) {
+        throw new UnauthorizedException('Credentials are not valid.');
+      }
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException('Credentials are not valid.');
+    }
+  }
+}
